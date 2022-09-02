e92f6a4a224daef43f247213b60ad21494bdd035
Kevin Hu <pkvinhu@gmail.com>
2022-09-02 13:46:13 -0500

    fix error handle

--- app.js
+++ app.js
@@ -7,36 +7,48 @@ const { sortByHourByType, getPointsByTrackId } = require("./utils/filter");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ error: err });
-});
+const handleError = (err, req, res) => {
+  res.status(500).send({ error: err.message });
+};
 
-app.get("/csvData", (req, res) => {
-  const json = csvToJsonList("data");
-  res.send(json);
-});
+app.get(
+  "/csvData",
+  (req, res) => {
+    const json = csvToJsonList("data");
+    res.send(json);
+  },
+  handleError
+);
 
-app.get("/volumes-by-hour/:type", (req, res) => {
-  // class of track
-  const { type } = req.params;
-  try {
-    // filter by hour for unique trackids matching the specific type
-    const volumesByHour = sortByHourByType(type);
-    res.send(volumesByHour);
-  } catch (err) {
-    next(err);
-  }
-});
+app.get(
+  "/volumes-by-hour/:type",
+  (req, res, next) => {
+    // class of track
+    const { type } = req.params;
+    try {
+      // filter by hour for unique trackids matching the specific type
+      const volumesByHour = sortByHourByType(type);
+      res.send(volumesByHour);
+    } catch (err) {
+      next(err);
+    }
+  },
+  handleError
+);
 
-app.get("/track-points/:trackid", (req, res) => {
-  const { trackid } = req.params;
-  try {
-    const trackPointsPerId = getPointsByTrackId(trackid);
-    res.send(trackPointsPerId);
-  } catch (err) {
-    next(err);
-  }
-});
+app.get(
+  "/track-points/:trackid",
+  (req, res, next) => {
+    const { trackid } = req.params;
+    try {
+      const trackPointsPerId = getPointsByTrackId(trackid);
+      res.send(trackPointsPerId);
+    } catch (err) {
+      next(err);
+    }
+  },
+  handleError
+);
 
 app.listen(PORT, function (err) {
   if (err) console.log(err);
